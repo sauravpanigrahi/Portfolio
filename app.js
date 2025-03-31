d1c804ad3b28eb9fe6d19b7fa77051a6fbe8978f
Saurav Panigrahi <sauravpanigrahi2004@gmail.com>
2025-03-31 21:15:30 +0530

    error handeling

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ require("dotenv").config();
 const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/portfolio";
 const homeRouter = require("./routes/home.js"); // Home-related routes start with /home
 
-// Contact-related routes start with /contact
-const contactRouter = require("./routes/contact.js");
+
 
 
 main()
@@ -74,7 +73,7 @@ app.use((req,res,next)=>{
     next();
 });
 app.use("/home", homeRouter);
-app.use("/contact", contactRouter); 
+
 app.get("/",(req,res)=>{
     res.redirect("/home");
  });
